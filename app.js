c2812f7c2a51d4be20a0d7443a9427c57e396b5a
ChinguSangho <sangho.grolleman@gmail.com>
2020-04-17 14:04:13 +0200

    JS - added finish game logic

--- app.js
+++ app.js
@@ -1,39 +1,68 @@
-const cards = Array.from(document.getElementsByClassName('card'));
 let allowedTurns = 2;
 let cardValueTurnOne, cardValueTurnTwo;
-let firstTurn = false;
+let numberOfMoves = 0;
+let matchesMade = 0;
+let level = 1;
 
+//collect all cards into an array and determine # of matches
+let cards = Array.from(document.getElementsByClassName('card'));
+let totalMatches = Math.floor(cards.length/2);
+
+//add event listener to all cards
 cards.forEach(card => {
     card.addEventListener('click', checkTurn);
 })
 
+//turn card logic
 function checkTurn() {
-
+    //check if card is first card
     if(allowedTurns == 2 && this.classList.contains('clickable')) {
+
+        //If first card: make card visible, prevent second click, store value in variable and decrease allowedTurns
         this.classList.remove('clickable');
         this.classList.add('visible');
         cardValueTurnOne = this;
         allowedTurns--;
-        console.log(allowedTurns, cardValueTurnOne);
+
     } else
+
+    //check if card is second card
     if(allowedTurns == 1 && this.classList.contains('clickable')) {
+        
+        //If second card: make card visible, prevent second click, store value in variable and decrease allowedTurns
         this.classList.remove('clickable');
         this.classList.add('visible');
         cardValueTurnTwo = this;
-        console.log(allowedTurns, cardValueTurnTwo,'Dit was de laatste');
         allowedTurns--;
        
-        
+            //evaluate cards and check for match
             if(cardValueTurnOne.textContent === cardValueTurnTwo.textContent) {
+                
+                //If match, leave cards as is (visible, not clickable) and reset allowedTurns
                 setTimeout(() => {
                     allowedTurns = 2;
-                }, 1500);
+                    //check if game is finished
+                    if(matchesMade === totalMatches) {
+
+                        alert('GG');
+                        //trigger game over screen
+                    }
+                }, 800);
+
+                matchesMade++;
+                console.log(matchesMade);
+
             } else {
+                
+                //If no match, make cards invisible and clickable again
                 setTimeout(() => {
+
                     cardValueTurnTwo.classList.remove('visible');
                     cardValueTurnOne.classList.remove('visible');
                     allowedTurns = 2;
+
                 }, 1500);
+
                 cardValueTurnOne.classList.add('clickable');
                 cardValueTurnTwo.classList.add('clickable');
                 
@@ -41,10 +70,25 @@ function checkTurn() {
         }
     }
 
+//Shuffle cards randomly
+function shuffle(){
+
+    var parent = $(".memory-game");
+    var divs = parent.children();
+    while (divs.length) {
+
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+
+    }
+}
+
+/* Code for button shuffle - maybe when using layover
+<button id="card">shuffle</button>
+
 document.getElementById("card").addEventListener("click", (function () {
     var parent = $(".memory-game");
     var divs = parent.children();
     while (divs.length) {
         parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
     }
-}) );
+}) ); */
