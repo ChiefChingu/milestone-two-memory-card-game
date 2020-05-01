79e459418648835a1220ab4f46d07f5ce0457cbc
ChinguSangho <sangho.grolleman@gmail.com>
2020-05-01 15:59:28 +0200

    Add mobile menu: JS logic done.

---
+++ assets/scripts/MobileMenu.js
@@ -0,0 +1,20 @@
+class MobileMenu {
+  constructor() {
+    this.menuIcon = document.querySelector(".site-header__menu-icon")
+    this.menuIconMiddle = document.querySelector(".site-header__menu-icon__middle")
+    this.menuContent = document.querySelector(".site-header__overlay")
+    this.events()
+  }
+
+  events() {
+    this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+  }
+
+  toggleTheMenu() {
+    this.menuContent.classList.toggle("site-header__overlay--is-visible")
+    this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
+    this.menuIconMiddle.classList.toggle("site-header__menu-icon__middle--close-x")
+  }
+}
+
+new MobileMenu();
