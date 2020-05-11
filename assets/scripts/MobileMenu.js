42407aaceba674e0a45ad378005d4d7b1f846505
ChinguSangho <sangho.grolleman@gmail.com>
2020-05-11 17:20:12 +0200

    Add whitespace for readability.

--- assets/scripts/MobileMenu.js
+++ assets/scripts/MobileMenu.js
@@ -1,40 +1,56 @@
 class MobileMenu {
+
   constructor() {
+
     this.menuIcon = document.querySelector(".site-header__menu-icon")
     this.menuIconMiddle = document.querySelector(".site-header__menu-icon__middle")
     this.menuContent = document.querySelector(".site-header__overlay")
     this.events()
+
   }
 
   events() {
+
     this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+
   }
 
   toggleTheMenu() {
+
     this.menuContent.classList.toggle("site-header__overlay--is-visible")
     this.menuIcon.classList.toggle("site-header__menu-icon--close-x")
     this.menuIconMiddle.classList.toggle("site-header__menu-icon__middle--close-x")
+
   }
+
 }
 
 new MobileMenu();
 
 //emailJS
 function initEmailAccount(){
+
   emailjs.init('user_TEJ6qvErK3u1SnFLlG4Gl');
+
 }
 
 function thankYou() {
+
   document.getElementById("contact-form").id = "contact-form-invisible";
   document.getElementById("contact-form-thank-you").id = "contact-form-thank-you-visible";
+
 }
 
 function initEmailCode() {
+
   document.getElementById('contact-form').addEventListener('submit', function(event) {
+
       event.preventDefault();
+
       // generate the contact number value
       this.contact_number.value = Math.random() * 100000 | 0;
       emailjs.sendForm('ms2_contact', 'template_2hqFHke2', this);
+      
       thankYou();
   });
 }
--- assets/scripts/highscore.js
+++ assets/scripts/highscore.js
@@ -1,29 +1,31 @@
 let lastGame = localStorage.getItem("lastGameLocal");
 let bestScoreInt = parseInt(localStorage.getItem("storedBestScore"), 10);
 
-if(localStorage.getItem("game A") === null) {
+if(localStorage.getItem("game A") === null) { //In case first play or reset highscore
     
     localStorage.setItem("game A", "done"); //Make game A not being null.
     localStorage.setItem("storedBestScore",lastGame); //Only score = best score.
     document.getElementById("highscore").innerHTML = lastGame; //Display best score.
 
-} else if(parseInt(lastGame,10) < bestScoreInt) { //Check if score is better than storedBestScore: must be integers. If so:
+    } else if(parseInt(lastGame,10) < bestScoreInt) { //Check if score is better than storedBestScore: must be integers. If so:
         
         localStorage.setItem("storedBestScore", lastGame); //storedBestScore is now the lastGame score.
-        document.getElementById("highscore").innerHTML = localStorage.getItem("storedBestScore"); //Display new best score.
         document.getElementById("new-highscore").innerHTML = "You did it! A new highscore! Woot!!";
+        document.getElementById("highscore").innerHTML = localStorage.getItem("storedBestScore"); //Display new best score.
         
     } else if(parseInt(lastGame,10) === bestScoreInt) { //Same score as best score
+
         document.getElementById("draw").innerHTML = "You are as good as your, ehm, self? Try again!";
         document.getElementById("highscore").innerHTML = bestScoreInt; //Display old highscore.
-    }
-    
+
+    }  
     
     else {
+
         document.getElementById("no-highscore").innerHTML = "What was that?? Let's not record this. Hurry, try again!";
         document.getElementById("highscore").innerHTML = bestScoreInt; //Display old highscore.
         
-    }
+}
 
 document.getElementById("lastscore").innerHTML = lastGame;
 
@@ -31,11 +33,9 @@ document.getElementById("lastscore").innerHTML = lastGame;
 document.getElementById("reset-score").addEventListener("click", resetLocalStorage);
 
 function resetLocalStorage() {
+
     localStorage.clear();
     console.log("loser!");
+    
 }
 
-//Randomize messages - to do!
-//Draw
-//Highscore
-//No highscore
